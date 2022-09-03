ff22810ce4728ab67d41a9d1b21c81d228dc1887
Ildar <steel14@yandex.ru>
2022-09-03 17:51:21 +0500

    added Compare digits method

---
+++ test1/Program.cs
@@ -0,0 +1,11 @@
+﻿void CompareDigits()
+{
+    int x = new Random().Next(10, 100);
+    int x1 = x / 10;
+    int x2 = x % 10;
+    Console.WriteLine($"Сгенерированно случайное число {x}");
+    if (x1 > x2) Console.WriteLine(x1);
+    else if (x2 > x1) Console.WriteLine(x2);
+    else Console.WriteLine("Это палиндром");
+}
+CompareDigits();
