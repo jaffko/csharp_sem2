49600bc0977983db350c8bc1e61d22727542ba38
Ildar <steel14@yandex.ru>
2022-09-03 19:26:28 +0500

    added IfMuliple by 7 and 23 method

---
+++ test4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+void IfMultiple(int a)
+{
+    if (a % 7 == 0)
+    {
+        if (a % 23 == 0) 
+        Console.WriteLine("Да");
+    }
+    else Console.WriteLine("Нет");
+}
+
+Console.Write("Введите число: ");
+IfMultiple(int.Parse(Console.ReadLine()));
