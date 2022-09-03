d429f2f7c1179a5a71209a5154f1d01b2c301aa9
Ildar <steel14@yandex.ru>
2022-09-03 18:31:30 +0500

    added diff task

---
+++ test2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите 2 числа: ");
+try
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    if (a % b == 0) Console.WriteLine("Кратно");
+    else Console.WriteLine($"Не кратно, остаток {a % b}");
+}
+catch 
+{
+    
+    Console.WriteLine("Нужно вводить числа!");
+}
