ff123cd6224b35f68e598dd146fe6e66f81bfd00
Ildar <steel14@yandex.ru>
2022-09-03 19:32:27 +0500

    added IfSquare method

---
+++ test5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+void IfSquare(int a, int b)
+{
+    if (b > a)
+    {
+        int temp = b;
+        b = a;
+        a = temp;
+    }
+    if (b * b == a) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}
+
+Console.WriteLine("Введите два числа");
+IfSquare(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
