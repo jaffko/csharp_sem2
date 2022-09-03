b5984cd7d07ad0bd547a5633de16af31785d68f0
Ildar <steel14@yandex.ru>
2022-09-03 18:52:01 +0500

    added crop digits

---
+++ test3/Program.cs
@@ -0,0 +1,14 @@
+﻿int RandThree ()
+{
+    int x = new Random().Next(100, 1000);
+    Console.WriteLine($"Трехзначное число -> {x}");
+    return (x);
+}
+int CropSecond(int digit)
+{
+    int x3 = digit / 100;
+    int x1 = digit % 10;
+    return x3 * 10 + x1;
+}
+
+Console.WriteLine(CropSecond(RandThree()));
